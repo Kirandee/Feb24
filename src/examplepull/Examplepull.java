3c093118d3d04bd4dd02e2d76a712db7413fd73c
Kirandee <59750207+Kirandee@users.noreply.github.com>
2020-01-24 16:01:51 -0500

    Update Examplepull.java

--- src/examplepull/Examplepull.java
+++ src/examplepull/Examplepull.java
@@ -14,12 +14,13 @@ public class Examplepull {
 
     /**
      * @param args the command line arguments
-     * this is test branch
+     * this is test branch ----------- branch operation
      */
     public static void main(String[] args) {
         System.out.println("we are learning pull operation");
            System.out.println("we are learning pull again"); 
           System.out.println("fetch + pull again"); 
+      System.out.println("branch"); 
     }
     
 }
